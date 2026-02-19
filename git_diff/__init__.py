f4d6d0ddeb67994ff53ca20a69f85c4df606d200
vasu-cli <vasu-cli@users.noreply.github.com>
2026-02-19 04:40:38 +0000

    chore: release v0.1.2 [skip ci]

--- git_diff/__init__.py
+++ git_diff/__init__.py
@@ -1,6 +1,6 @@
 """git-diff -- Beautiful git viewer in your browser. Like GitHub, but local."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 __author__ = "Ankit Chaubey"
 __email__ = "ankitchaubey.dev@gmail.com"
 __url__ = "https://github.com/ankit-chaubey/git-diff"
